4ab1dec955825eae5b1b5e49a0c9190641d6ff8a
tseretelii <tsereteligigi01@gmail.com>
2024-11-20 21:58:14 +0400

    GuessingGame - Complete

--- MidtermProjects/Program.cs
+++ MidtermProjects/Program.cs
@@ -4,32 +4,35 @@
     {
         static void Main(string[] args)
         {
+            // N1
             #region Calculator
-            // Calculator
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
 
-            double us1;
-            double us2;
-            Console.WriteLine("Enter The First Number");
-            while (true)
-            {
-                if (double.TryParse(Console.ReadLine(), out us1))
-                    break;
-            }
-            Console.WriteLine("Enter The Second Number");
-            while (true)
-            {
-                if (double.TryParse(Console.ReadLine(), out us2))
-                    break;
-            }
+            //double us1;
+            //double us2;
+            //Console.WriteLine("Enter The First Number");
+            //while (true)
+            //{
+            //    if (double.TryParse(Console.ReadLine(), out us1))
+            //        break;
+            //}
+            //Console.WriteLine("Enter The Second Number");
+            //while (true)
+            //{
+            //    if (double.TryParse(Console.ReadLine(), out us2))
+            //        break;
+            //}
 
-            Console.WriteLine(Calculator.Addition(us1, us2));
+            //Console.WriteLine(Calculator.Addition(us1, us2));
             #endregion
+
+            // N2
+            GuessingGame.StartGame(5);
         }
     }
 
+    // N1
     #region Calculator
-    // Calculator
     public static class Calculator
     {
         public static double Addition(double x, double y)
@@ -50,4 +53,21 @@
         }
     }
     #endregion
+
+    // N2
+    public class GuessingGame
+    {
+        public static void StartGame(int range)
+        {
+            Random random = new Random();
+            int number = random.Next(0, range);
+
+            int guess;
+            while (!int.TryParse(Console.ReadLine(), out guess) || guess != number)
+            {
+                Console.WriteLine("Try Again");
+            }
+            Console.WriteLine($"You Win, The Number Was: {number}");
+        }
+    }
 }
