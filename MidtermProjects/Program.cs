6e1f178e3d7289bd2253ef6e55a15a73791628a3
tsugigiwereteli@gmail.com <tsugigiwereteli@gmail.com>
2024-11-26 14:33:47 +0400

    BookManager - AddBook() & GetAllBooks() - Done

--- MidtermProjects/Program.cs
+++ MidtermProjects/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Win32.SafeHandles;
+using System.Text;
+using System.Text.Json;
 
 namespace MidtermProjects
 {
@@ -20,7 +22,17 @@ namespace MidtermProjects
             #endregion
 
             // N3
-            Hangman.StartGame(Hangman.Difficulty.Hard);
+            #region Hangman
+            //Hangman.StartGame(Hangman.Difficulty.Hard);
+            #endregion
+
+            // N4 OOP
+            #region წიგნების სია
+
+            BookManager.AddBook("lord of the rings II", "j.r.r Tolkein", DateOnly.FromDateTime(DateTime.Now));
+            BookManager.GetAllBooks();
+
+            #endregion
 
         }
     }
@@ -292,42 +304,82 @@ namespace MidtermProjects
     #endregion
 
 
-    // OOP
+    // N4 OOP
     #region წიგნების სია
     public class Book
     {
         public Book(string title, string author, DateOnly releaseYear)
         {
-            _title = title;
-            _author = author;
-            _releaseYear = releaseYear;
+            Title = title;
+            Author = author;
+            ReleaseYear = releaseYear;
         }
-        private string _title;
-        private string _author;
-        private DateOnly _releaseYear;
+        public string Title { get; private set; }
+        public string Author { get; private set; }
+        public DateOnly ReleaseYear { get; private set; }
     }
 
-    public class BookManager
+    public static class BookManager
     {
 
-        public List<Book> Books { get; private set; }
-        private static string _path = "C:\\Users\\Gigi\\Desktop\\BookFolder";
+        public static List<Book> Books { get; private set; }
+        private static string _path = "C:\\Users\\User\\Desktop\\BookFolder";
         private static DirectoryInfo _directory = new DirectoryInfo(_path);
 
-        public void AddBook(string title, string author, DateOnly releaseYear)
+        public static void AddBook(string title, string author, DateOnly releaseYear)
         {
-            Books.Add(new Book(title, author, releaseYear));
-            // SaveToFile(Books[Books.Count-1])
+            //Books.Add(new Book(title, author, releaseYear));
+            SaveToFile(new Book(title, author, releaseYear));
         }
 
-        private void SaveToFile(Book book)
+        public static void GetAllBooks()
+        {
+            Books = new List<Book>();
+            FileInfo[] fileInfos = _directory.GetFiles();
+            foreach (FileInfo fileInfo in fileInfos)
+            {
+                using (FileStream fs = fileInfo.OpenRead())
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        Books.Add(JsonSerializer.Deserialize<Book>(sr.ReadToEnd()));
+                    }
+                }
+            }
+            Books.ForEach(x => Console.WriteLine($"\"{x.Title}\" By {x.Author}, Published in {x.ReleaseYear}"));
+        }
+
+        private static void SaveToFile(Book book)
         {
             if (!_directory.Exists)
             {
                 _directory.Create();
             }
+            string filePath = _directory.FullName + "\\" + $"{book.Title}.txt";
 
+            if (_directory.GetFiles($"{book.Title} - {book.Author}.txt").Count() > 0)
+            {
+                Console.WriteLine("This book is allready added");
+                return;
+            }
+            else if (_directory.GetFiles($"{book.Title}.txt").Count() > 0)
+            {
+                Console.WriteLine("Book With this title allready exists so we have to add the author to the file name as well");
+                filePath = _directory.FullName + "\\" + $"{book.Title} - {book.Author}.txt";
+            }
+
+            using(FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                using (StreamWriter streamWriter = new StreamWriter(fs))
+                {
+                    string serializedBook = JsonSerializer.Serialize(book);
+                    streamWriter.WriteLine(serializedBook);
+                    Console.WriteLine("Book Added!");
+                }
+            }
         }
+
+
     }
     #endregion
 }
