18695b2e1fd6652de6a411918e5f5867ee1c870c
tseretelii <tsereteligigi01@gmail.com>
2024-11-29 16:12:35 +0400

    Added some methods

--- MidtermProjects/AtmMachine.cs
+++ MidtermProjects/AtmMachine.cs
@@ -8,35 +8,44 @@ namespace MidtermProjects
 {
     public static class AtmMachine
     {
-        public static void RegisterPerson(string name, string secondName, string userName, string password)
+        public static void RegisterAccountForPerson(string name, string secondName)
         {
-            Person person = new Person(name.ToLower(), secondName.ToLower(), userName.ToLower(), password);
+            Person person = new Person(name, secondName);
+            BankAccount bankAccount = new BankAccount(person);
         }
+
+        //public static void DepositMoney()
     }
 
     internal class Person
     {
         public string Name { get; private set; }
         public string SecondName { get; private set; }
-        public string UserName { get; private set; }
-        private string Password { get; set; }
-        public string AccountNumber { get; private set; }
-        public Person(string name, string secondName, string userName, string password)
+        public Person(string name, string secondName)
         {
             Name = name;
             SecondName = secondName;
-            UserName = userName;
-            Password = password;
-            AccountNumber = SetAccountNumber();
         }
-
-        private string SetAccountNumber()
+    }
+    internal class BankAccount
+    {
+        public Person Person1 { get; set; }
+        public Dictionary<string, decimal> AccountNumber { get; private set; }
+        public int MyProperty { get; set; }
+        public BankAccount(Person person)
+        {
+            Person1 = person;
+            string accN = AccountNumberGenerator();
+            AccountNumber.Add(accN + "USD",0);
+            AccountNumber.Add(accN + "EUR",0);
+        }
+        private string AccountNumberGenerator()
         {
             Random random = new Random();
             string accountNumber = "";
             for (int i = 0; i < 16; i++)
             {
-                accountNumber += random.Next(0, 10).ToString();
+                accountNumber += random.Next(1, 10).ToString();
             }
             return accountNumber;
         }
