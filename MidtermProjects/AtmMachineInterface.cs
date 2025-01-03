d21907b14e61860d87735f1149e0d552bd24e8c0
tseretelii <tsereteligigi01@gmail.com>
2025-01-03 21:58:23 +0400

    Continue on pc

---
+++ MidtermProjects/AtmMachineInterface.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MidtermProjects
+{
+    public static class AtmMachineInterface
+    {
+        public static void StartAtmMachine()
+        {
+            string userChoice = Wellcome();
+            BankAccount account;
+
+            if (userChoice == "1")
+                account = UserLogIn();
+            else
+                account = UserRegister();
+
+            while (true)
+            {
+                userChoice = UserMenu();
+
+                if (userChoice == "1")
+                    CheckBalance(account);
+                else if (userChoice == "2")
+                    DepositMoney(account);
+                //else if (userChoice == "3")
+                //    // Transfer();
+                else
+                    break;
+            }
+
+        }
+
+        private static string Wellcome()
+        {
+            Console.WriteLine("Wellcome To LTD Gigi's Bank\n1. Login\n2. Register");
+            return RegexForInput.GetCheckedUserInput("^[12]$", "1. Login\n2. Register");
+        }
+
+        private static string UserMenu()
+        {
+            Console.WriteLine("What Would You Like To Do?\n1. Check Balance \n2. Deposit Money\n3. Transfer\n4. Log Out");
+
+            return RegexForInput.GetCheckedUserInput("^[1-4]$", "1. Check Balance \n2. Deposit Money\n3. Transfer\n4. Log Out");
+        }
+
+        // For now user logs in with their name and personal number
+        private static BankAccount UserLogIn()
+        {
+
+            Console.WriteLine("Please enter your name\n Name: ");
+
+            string name = RegexForInput.GetCheckedUserInput("^[a-zA-Z]+$", "Name: ");
+
+            Console.WriteLine("Please enter your personal number: ");
+
+            string personalN = RegexForInput.GetCheckedUserInput("^\\d{11}$", "Personal number: ");
+            
+            return Recorder.GetBankAccount(Recorder.GetPerson(personalN)); // if user doesn't exist i have to handle it!!!!
+        }
+
+        private static BankAccount UserRegister()
+        {
+            Console.WriteLine("Please enter your name\nName: ");
+
+            string name = RegexForInput.GetCheckedUserInput("^[a-zA-Z]+$", "Name: ");
+
+            string surName = RegexForInput.GetCheckedUserInput("^[a-zA-Z]+$", "Second Name: ");
+
+            string personalN = RegexForInput.GetCheckedUserInput("^\\d{11}$", "Personal number: ");
+
+            Person person = new Person(name, surName, personalN);
+
+            return AtmMachine.RegisterAccountForPerson(person);
+        }
+
+        private static void CheckBalance(BankAccount account)
+        {
+            BankAccount bankAccount = Recorder.GetBankAccount(account.PersonInfo);
+            foreach (AccountIBAN iban in bankAccount.AccountNumber)
+            {
+                foreach (KeyValuePair<Currency, decimal> curr in iban.Balance)
+                {
+                    Console.WriteLine($"{curr.Key} - {curr.Value}");
+                }
+            }
+        }
+
+        private static void DepositMoney(BankAccount account)
+        {
+            Console.WriteLine("Which IBAN?");
+            
+            for (int i = 1; i < account.AccountNumber.Count + 1; i++)
+            {
+                Console.WriteLine($"{i}. {account.AccountNumber[i - 1].AccNum}");
+            }
+
+            int ibanIndex = int.Parse(RegexForInput.GetCheckedUserInput($"^[1{account.AccountNumber.Count}]$"));
+
+            ibanIndex--;
+
+            Console.WriteLine("Please enter the amount:");
+
+            decimal amount = decimal.Parse(RegexForInput.GetCheckedUserInput("^-?\\d+(\\.\\d+)?$"));
+
+            var keys = account.AccountNumber[ibanIndex].Balance.Keys.ToList();
+
+            for (int i = 1;i < account.AccountNumber[ibanIndex].Balance.Keys.Count; i++)
+            {
+                Console.WriteLine($"{i}. {account.AccountNumber[ibanIndex].Balance.Keys.ToList()[i - 1]}");
+            }
+
+            int currIndex = int.Parse(RegexForInput.GetCheckedUserInput($"^[1{account.AccountNumber[ibanIndex].Balance.Keys.Count}]$"));
+
+            AtmMachine.DepositFunds(account, account.AccountNumber[ibanIndex], amount, account.AccountNumber[ibanIndex].Balance.Keys.ToList()[currIndex - 1]);
+            
+            Console.WriteLine("Complete!");
+        }
+    }
+
+    internal static class RegexForInput
+    {
+        internal static string GetCheckedUserInput(string regex, string userChoicesForInvalidInput = "")
+        {
+            Regex r = new Regex(regex);
+
+            string userInput;
+
+            while (true)
+            {
+                userInput = Console.ReadLine();
+                if (r.IsMatch(userInput))
+                {
+                    return userInput;
+                }
+                Console.WriteLine("Invalid Input\nTry Again: " + "\n" + userChoicesForInvalidInput);
+            }
+
+        }
+    }
+}
--- MidtermProjects/Program.cs
+++ MidtermProjects/Program.cs
@@ -36,23 +36,25 @@ namespace MidtermProjects
 
             //AtmMachine.CreateTransaction(bank1, bank2, 50, Currency.GEL).ExecuteTransaction(0,0); // The indices 0 and 0 indicate the sender's and receiver's accounts involved in the transaction.
 
-            
-            Person person1 = new Person("Gigi", "Tsereteli", "12345678911");
-            var acc1 = AtmMachine.RegisterAccountForPerson(person1);
-            if (acc1 == null )
-            {
-                acc1 = Recorder.GetBankAccount(Recorder.GetPerson(person1.PersonalN));
-            }
-            Person person2 = new Person("Elene", "Tsereteli", "12345678910");
-            var acc2 = AtmMachine.RegisterAccountForPerson(person2);
-            if (acc2 == null)
-            {
-                acc2 = Recorder.GetBankAccount(Recorder.GetPerson(person2.PersonalN));
-            }
 
-            //AtmMachine.DepositFunds(acc1, acc1.AccountNumber[0], 100, Currency.GEL);
+            //Person person1 = new Person("Gigi", "Tsereteli", "12345678911");
+            //var acc1 = AtmMachine.RegisterAccountForPerson(person1);
+            //if (acc1 == null )
+            //{
+            //    acc1 = Recorder.GetBankAccount(Recorder.GetPerson(person1.PersonalN));
+            //}
+            //Person person2 = new Person("Elene", "Tsereteli", "12345678910");
+            //var acc2 = AtmMachine.RegisterAccountForPerson(person2);
+            //if (acc2 == null)
+            //{
+            //    acc2 = Recorder.GetBankAccount(Recorder.GetPerson(person2.PersonalN));
+            //}
 
-            AtmMachine.CreateTransaction(acc1, acc2, 50, Currency.GEL, 0, 0).ExecuteTransaction();
+            ////AtmMachine.DepositFunds(acc1, acc1.AccountNumber[0], 100, Currency.GEL);
+
+            //AtmMachine.CreateTransaction(acc1, acc2, 50, Currency.GEL, 0, 0).ExecuteTransaction();
+
+            AtmMachineInterface.StartAtmMachine();
             #endregion
         }
     }
